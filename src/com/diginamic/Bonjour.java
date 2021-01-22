0f5d6300d64d253797ef4b02195c3f93317fa17c
Alexis <alexis.goupillot.dev@gmail.com>
2021-01-22 16:05:31 +0100

    Added Bonjour

---
+++ src/com/diginamic/Bonjour.java
@@ -0,0 +1,14 @@
+package com.diginamic;
+
+public class Bonjour {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		long a = 3000000000L;
+		float b = 1.8F;
+		final int c = 5;
+		System.out.println(b);
+	}
+
+}

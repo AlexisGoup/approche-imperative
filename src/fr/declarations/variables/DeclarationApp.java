e6ffa0c7d8f7d3d3cbc399ad3215252c9f48970f
Alexis <alexis.goupillot.dev@gmail.com>
2021-01-22 15:53:46 +0100

    V1

---
+++ src/fr/declarations/variables/DeclarationApp.java
@@ -0,0 +1,28 @@
+package fr.declarations.variables;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 125;
+		short b = 30000; 
+		int c = 1000000;
+		long d = 1000000000;
+		float e = 1.2484484F;
+		double f = 151515146515464D;
+		
+		long v = b/d;
+		
+		boolean h = true;
+		
+		char g = 'A';
+		
+		String i = "Hola senorita";
+		String randmonString = "Voici le resultat d'un calcul : 1 + 5 = 6 \n";
+		
+		System.out.println(i);
+		System.out.println(randmonString);
+		System.out.println(v);
+	}
+
+}
